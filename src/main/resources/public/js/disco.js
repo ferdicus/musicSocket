b17d6a252be9dcaa90ba48382067443ba8f71ed4
Your Name <you@example.com>
2019-02-21 15:31:00 +0100

    add disco grid

--- src/main/resources/public/js/disco.js
+++ src/main/resources/public/js/disco.js
@@ -1,15 +1,22 @@
 const url = `ws://${window.location.host}/disco`
 
 const connection = new WebSocket(url);
+const memberTiles = $('#grid').children();
 
 connection.onopen = () => {
   console.log('--- connection onopen');
-}
+};
 
 connection.onerror = err => {
   console.error('--- connection onerror', err);
-}
+};
 
 connection.onmessage = e => {
-  console.log('--- connection onmessage: ', e.data)
-}
+  const currentBandMembers = JSON.parse(e.data)
+  console.log('--- connection onmessage: ', e.data);
+
+  $.each(memberTiles, (index, tile) => {
+    tile.css({'backgroundColor': currentBandMembers[index].color})
+    if (currentBandMembers[index].playing) tile.text(♫);
+  });
+};
