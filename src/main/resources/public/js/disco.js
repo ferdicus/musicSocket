8c2ef48ee48aadbdd17c5b34437ad0902c6d7baf
Your Name <you@example.com>
2019-02-21 15:46:56 +0100

    break loop when no more members

--- src/main/resources/public/js/disco.js
+++ src/main/resources/public/js/disco.js
@@ -15,8 +15,11 @@ connection.onmessage = e => {
   const currentBandMembers = e.data
   console.log('--- connection onmessage: ', e.data);
 
-  $.each(memberTiles, index => {
-    $(this).css({'backgroundColor': currentBandMembers[index].color})
-    if (currentBandMembers[index].playing) $(this).text('♫');
-  });
+  if (currentBandMembers) {
+    $.each(memberTiles, (index, tile) => {
+      if (!currentBandMembers[index]) return false;
+      $(tile).css({'backgroundColor': currentBandMembers[index].color})
+      if (currentBandMembers[index].playing) $(tile).text('♫');
+    });
+  }
 };
