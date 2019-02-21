a2095d9267563b8b2d46fcf5bea98cca3830ae00
Your Name <you@example.com>
2019-02-21 11:45:06 +0100

    Add initial ws setup

---
+++ src/main/resources/public/js/main.js
@@ -0,0 +1,17 @@
+const url = `wss://${window.location}:${window.port}/midi`
+
+console.log('url', url)
+
+const connection = new WebSocket(url);
+
+connection.onopen = () => {
+  console.log('--- connection onopen');
+}
+
+connection.onerror = err => {
+  console.error('--- connection onerror', err);
+}
+
+connection.onmessage = e => {
+  console.log('--- connection onmessage: ', e.data)
+}
